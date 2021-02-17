5250359703f43a1ef70df43f7a784277a3c8ad35
bcheney21 <brinn_cheney@icloud.comgit config --global user.email brinn_cheney@icloud.com>
2021-02-17 11:51:14 -0800

    updates

--- part-4.sql
+++ part-4.sql
@@ -19,11 +19,17 @@ SELECT name FROM properties LIMIT 3;
 -- 10. Show all the properties that are owned by the owner with an id of 2.
 
 -- 11. Show all the properties that are owned by Yuki.
-
+SELECT * FROM 
+owners JOIN properties
+ON owners.id = properties.owner_id
+WHERE owners.name = 'Yuku';
 -- 12. Show the count of how many properties Yuki owns.
 
 -- 13. Show the name of Willowspring's owner.
-
+SELECT owners.name FROM
+owners JOIN properties
+ON owners.id = properties.owner_id
+WHERE properties.name = 'Willowspring';
 -- 14. Show the names of the people who own properties that have names of 10 characters or longer
 
 -- 15. Change Willowspring so that is now owned by Yuki.
