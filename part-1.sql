dbd08df81cf1b33ce8db19d3a9130ebf5aca91a7
CHRISDAVIDSPICER <CHRISDAVIDSPICER@GMAIL.COM>
2021-02-17 11:32:55 -0800

    Finish apartment sql

--- part-1.sql
+++ part-1.sql
@@ -1,9 +1,20 @@
 -- 1. create a database called apartments
+CREATE DATABASE apartments;
 
 -- 2. connect to that database
+\c apartments
 
 -- 3. create the owners table
+CREATE TABLE owners (
+      id SERIAL PRIMARY KEY,
+      name text,
+      age int
+);
 
 -- 4. create the properties table
-
+CREATE TABLE properties (
+      id SERIAL PRIMARY KEY,
+      name text,
+      units int
+);
 
