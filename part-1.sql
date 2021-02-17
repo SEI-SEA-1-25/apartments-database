a17ca7b09a9d994288b59e6636a061ff0e8c1be4
bcheney21 <brinn_cheney@icloud.comgit config --global user.email brinn_cheney@icloud.com>
2021-02-17 11:31:40 -0800

    mostly finished

--- part-1.sql
+++ part-1.sql
@@ -1,9 +1,15 @@
 -- 1. create a database called apartments
-
+CREATE DATABASE apartments;
 -- 2. connect to that database
-
+\c apartments;
 -- 3. create the owners table
-
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    age INTEGER);
 -- 4. create the properties table
-
+CREATE TABLE properties (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    units INTEGER);
 
--- part-4.sql
+++ part-4.sql
@@ -1,17 +1,17 @@
 -- 1. Show all the data in the owners table.
-
+SELECT * FROM owners;
 -- 2. Show the names of all owners.
-
+SELECT name FROM owners;
 -- 3. Show the ages of all of the owners in ascending order.
-
+SELECT * FROM owners ORDER BY age ASC;
 -- 4. Show the names and ages of all owners who are older than 30.
-
+SELECT * FROM owners WHERE age > 30;
 -- 5. Change Jane's age to 30.
-
+UPDATE owners SET age = 30 WHERE name = 'Jane';
 -- 6. Change Jane's name to Janet.
-
+UPDATE owners SET name = 'Janet' WHERE name = 'Jane';
 -- 7. Show the names of the first three properties in your properties table.
-
+SELECT name FROM properties LIMIT 3;
 -- 8. Show the name of all properties whose name contains an `a`.
 
 -- 9. Show the highest age of all owners.
