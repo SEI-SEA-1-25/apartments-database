0ad9048969aa97fc9f9c9470cef601cdf790793a
wonkatin <j.drawers@gmail.com>
2021-02-17 13:50:46 -0600

    almost done with part 4

--- part-3.sql
+++ part-3.sql
@@ -1,4 +1,26 @@
 -- Insert Data into the owners table
+INSERT INTO owners (
+    name,
+    age
+)
+VALUES ('William', 29),
+('Jane', 43),
+('Yuki', 67),
+('Sarah', 35),
+('Justin', 30),
+('Sean', 22);
 
 -- Insert Data into the properties table
 
+INSERT INTO properties (
+    name,
+    units,
+    ownerID
+)
+VALUES
+( 'Archstone', 20, 3),
+( 'Zenith Hills', 10, 3),
+( 'Willowspring', 30, 2),
+( 'EchoValley', 15, 6),
+( 'Fern Gully', 40, 5),
+( 'Magnolia Fortress', 2, 4);
--- part-4.sql
+++ part-4.sql
@@ -1,13 +1,13 @@
 -- 1. Show all the data in the owners table.
-
+SELECT * FROM owners;
 -- 2. Show the names of all owners.
-
+SELECT name FROM owners;
 -- 3. Show the ages of all of the owners in ascending order.
-
+SELECT age FROM owners ORDER BY age ASC;
 -- 4. Show the names and ages of all owners who are older than 30.
-
+SELECT name, age FROM owners WHERE age > 30;
 -- 5. Change Jane's age to 30.
-
+UPDATE owners SET age = 30 WHERE name = 'Jane';
 -- 6. Change Jane's name to Janet.
 
 -- 7. Show the names of the first three properties in your properties table.
@@ -17,13 +17,19 @@
 -- 9. Show the highest age of all owners.
 
 -- 10. Show all the properties that are owned by the owner with an id of 2.
-
+SELECT * FROM properties WHERE ownerID = 2;
 -- 11. Show all the properties that are owned by Yuki.
-
+SELECT * FROM 
+owners JOIN properties
+ON properties.ownerID = owners.id 
+WHERE owners.name = 'Yuki';
 -- 12. Show the count of how many properties Yuki owns.
 
 -- 13. Show the name of Willowspring's owner.
-
+SELECT owners.name FROM 
+owners JOIN properties 
+ON properties.ownerID = owners.id
+WHERE properties.name = 'Willowspring';
 -- 14. Show the names of the people who own properties that have names of 10 characters or longer
 
 -- 15. Change Willowspring so that is now owned by Yuki.
